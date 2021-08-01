9deeb4cee231bd8779d77dd1a1cc3d8269e67441
Devendra Singh <sdev6631@gmail.com>
2021-08-01 17:30:00 +0530

    tt

--- tickets/schema.py
+++ tickets/schema.py
@@ -1,11 +1,23 @@
 import graphene
 from graphene_django import DjangoObjectType, fields
 from .models import *
+from graphene import relay
 
 class TicketsType(DjangoObjectType):
     class Meta:
         model = Ticket
-        fields = ['ticket_no', 'seat_np']
+
+class ScreenType(DjangoObjectType):
+
+    class Meta: 
+        model = Screen
+        fields = ('__all__')
+        interfaces = (relay.Node,)
+
 
 class Query(graphene.ObjectType):
     get_tickets = graphene.List(TicketsType)
+    get_screens = graphene.List(ScreenType)
+
+    def resolve_get_screens(root, info):
+        return Screen.objects.all()
